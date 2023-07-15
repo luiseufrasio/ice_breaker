02201481c11319bfb47de3a024888e1e8816b11c
luiseufrasio <luis.eufrasio@gmail.com>
2023-07-15 09:03:03 -0300

    consuming linkedin API

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,6 +2,8 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
+from third_parties.linkedin import scrape_linkedin_profile
+
 information = """
 Elon Reeve Musk FRS (Pretória, 28 de junho de 1971) é um empreendedor,[3] empresário e filantropo sul-africano-canadense, naturalizado norte-americano. Ele é o fundador, diretor executivo e diretor técnico da SpaceX; CEO da Tesla, Inc.; vice-presidente da OpenAI, fundador e CEO da Neuralink; cofundador, presidente da SolarCity e proprietário do Twitter. Em dezembro de 2022, tinha uma fortuna avaliada em US$ 139 bilhões de dólares, tornou-se a segunda pessoa mais rica do mundo, de acordo com a Bloomberg, atrás apenas do empresário Jeff Bezos.[4][5][6]
 
@@ -15,18 +17,22 @@ Musk tem sido alvo de críticas devido a posturas incomuns ou não científicas 
 if __name__ == "__main__":
     print("Hey LangChain!")
 
-    summary_template = """
-        given the LinkedIn information {information} about a person from I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-    """
-
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+    # summary_template = """
+    #     given the LinkedIn information {information} about a person from I want you to create:
+    #     1. a short summary
+    #     2. two interesting facts about them
+    # """
+    #
+    # summary_prompt_template = PromptTemplate(
+    #     input_variables=["information"], template=summary_template
+    # )
+    #
+    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    #
+    # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    #
+    # print(chain.run(information=information))
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url="https://www.linkedin.com/in/luis-neto-16786b227/"
     )
-
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-
-    print(chain.run(information=information))
+    print(linkedin_data.json())
