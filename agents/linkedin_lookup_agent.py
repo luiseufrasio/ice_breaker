4b2682f626b6f0f292507a050ad85f2c797c622f
luiseufrasio <luis.eufrasio@gmail.com>
2023-07-20 15:29:14 -0300

    template fixed

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -6,7 +6,7 @@ from tools.tools import get_profile_url
 
 def lookup(name: str) -> str:
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    template = """given the full name {name} I want you to get it me a link to their Linkedin profile page.
+    template = """given the full name {name_of_person} I want you to get it me a link to their Linkedin profile page.
                             Your answer should contain only a URL"""
 
     tools_for_agent = [
