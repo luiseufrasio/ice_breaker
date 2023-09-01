9906ae37a419f5d2b0f3d4d7857193bbbc39af32
luiseufrasio <luis.eufrasio@gmail.com>
2023-09-01 17:33:40 -0300

    added output parser

---
+++ output_parsers.py
@@ -0,0 +1,25 @@
+from langchain.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class PersonIntel(BaseModel):
+    summary: str = Field(description="Summary of th person")
+    facts: List[str] = Field(description="Interesting facts about the person")
+    topics_of_interest: List[str] = Field(
+        description="Topics that may interest the person"
+    )
+    ice_breakers: List[str] = Field(
+        description="Create the ice breakers to open a conversation with a person"
+    )
+
+    def to_dict(self):
+        return {
+            "summary": self.summary,
+            "facts": self.facts,
+            "topics_of_interest": self.topics_of_interest,
+            "ice_breakers": self.ice_breakers,
+        }
+
+
+person_intel_parser = PydanticOutputParser(pydantic_object=PersonIntel)
